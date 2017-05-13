0b7cfc7441a4c2f164d727c9e18e1f73f5790c4b
ashmoreinc <ashmoreinc@ashmoreinc-PC.localdomain>
2017-05-13 23:11:47 +0100

    Added a 'next ip' procedure and the main conditional operator overides

--- IP.py
+++ IP.py
@@ -51,3 +51,145 @@ class IPV4:
 				return False
 
 		return True
+
+	def Next_IP (self):
+		output = self.Current_IP
+		#	Check the last position
+		if output[3] < 255:
+			output[3] += 1
+		else:
+			output[3] = 0
+			# Check the penultimate position
+			if output[2] < 255:
+				output[2] += 1
+			else:
+				output[2] = 0
+				# Check 2nd position
+				if output[1] < 255:
+					output[1] += 1
+				else:
+					output[1] = 0
+					# Check 1st position
+					if output[0] < 255:
+						output[0] += 1
+					else:
+						return False
+		self.Current_IP = output
+		return True
+
+	# Expressional Operators below
+
+	def __gt__ (self, IP2): # this > other
+		# Check if compared to another IP
+		if type(IP2) != IP:
+			raise TypeError("IP's can only be compared with another IP.")
+		this_ip = self.Current_IP
+		that_ip = IP2.Current_IP
+
+		# Check if both values are verified
+		if not self.Verify_IP():
+			raise TypeError("This IP has an invalid format")
+		if not IP2.Verify_IP():
+			raise TypeError("Second IP has an invalid format")
+
+		# Loop through each place in the IP, if gt return true, if equal move on to the next index
+		for index in range(4):
+			if this_ip[index] > that_ip[index]:
+				return True
+			elif that_ip[index] > this_ip[index]:
+				return False
+
+		# At this point both values should be equal so return False
+		return False
+
+	def __ge__ (self, IP2): # this >= other
+		# Check if compared to another IP
+		if type(IP2) != IP:
+			raise TypeError("IP's can only be compared with another IP.")
+		this_ip = self.Current_IP
+		that_ip = IP2.Current_IP
+
+		# Check if both values are verified
+		if not self.Verify_IP():
+			raise TypeError("This IP has an invalid format")
+		if not IP2.Verify_IP():
+			raise TypeError("Second IP has an invalid format")
+
+		# Loop through each place in the IP, if gt return true, if equal move on to the next index
+		for index in range(4):
+			if this_ip[index] > that_ip[index]:
+				return True
+			elif that_ip[index] > this_ip[index]:
+				return False
+
+		# At this point both values should be equal so return True
+		return True
+
+	def __le__ (self, IP2): # this <= other
+		# Check if compared to another IP
+		if type(IP2) != IP:
+			raise TypeError("IP's can only be compared with another IP.")
+		this_ip = self.Current_IP
+		that_ip = IP2.Current_IP
+
+		# Check if both values are verified
+		if not self.Verify_IP():
+			raise TypeError("This IP has an invalid format")
+		if not IP2.Verify_IP():
+			raise TypeError("Second IP has an invalid format")
+
+		# Loop through each place in the IP, if gt return true, if equal move on to the next index
+		for index in range(4):
+			if this_ip[index] < that_ip[index]:
+				return True
+			elif that_ip[index] < this_ip[index]:
+				return False
+
+		# At this point both values should be equal so return True
+		return True
+
+	def __lt__ (self, IP2): # this < other
+		# Check if compared to another IP
+		if type(IP2) != IP:
+			raise TypeError("IP's can only be compared with another IP.")
+		this_ip = self.Current_IP
+		that_ip = IP2.Current_IP
+
+		# Check if both values are verified
+		if not self.Verify_IP():
+			raise TypeError("This IP has an invalid format")
+		if not IP2.Verify_IP():
+			raise TypeError("Second IP has an invalid format")
+
+		# Loop through each place in the IP, if gt return true, if equal move on to the next index
+		for index in range(4):
+			if this_ip[index] < that_ip[index]:
+				return True
+			elif that_ip[index] < this_ip[index]:
+				return False
+
+		# At this point both values should be equal so return True
+		return False
+
+	def __eq__ (self, IP2): # this == other
+		# Check if compared to another IP
+		if type(IP2) != IP:
+			raise TypeError("IP's can only be compared with another IP.")
+		this_ip = self.Current_IP
+		that_ip = IP2.Current_IP
+
+		# Check if both values are verified
+		if not self.Verify_IP():
+			raise TypeError("This IP has an invalid format")
+		if not IP2.Verify_IP():
+			raise TypeError("Second IP has an invalid format")
+
+		# Loop through each place in the IP, if gt return true, if equal move on to the next index
+		for index in range(4):
+			if this_ip[index] > that_ip[index]:
+				return False
+			elif that_ip[index] > this_ip[index]:
+				return False
+
+		# At this point both values should be equal so return True
+		return True
