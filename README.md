afdf38872e6b7de63b34dd89ec62d597797caa88
ashmoreinc <ashmoreinc@ashmoreinc-PC.localdomain>
2017-05-14 14:34:38 +0100

    Scanner functionality added, along with a small worked example.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 IP-Scanner in pure python.
 
 IP.py: A module which holds a new data type (IPV4) to store IPV4 addresses, allowing for comparisons, moving onto the next ip and verifying whether the IP that has been used/input is valid.
+
+Scanner.py: A module which holds an object in which will control all of the running of the Scanner.
--- Scanner.py
+++ Scanner.py
@@ -1,15 +1,25 @@
+from IP import *
+from queue import Queue
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+
 class Scan_Handler:
 	def __init__ (self, ports=[80], threads=10, verbose=False, verbosity="low"):
 		# Verbosity Settings
 		self.Verbose 	= verbose
-		self.Verbosity 	= verbosity_level # Can be low, Medium, High
+		self.Verbosity 	= verbosity # Can be low, Medium, High
 
 		# Scan Options
 		self.Ports = ports
-		self.Threads = threads
+		self.Thread_Size = threads
 
 		# Runtime Variables
 		self.Running = False
+		self.que = Queue() # This will be where all the IP's are pulled from and stored to before running
+		self.Threads = {} # This will be a list of threads open {"thread num":Thread}
+		
+		# Results
+		self.Open_Addresses = {} # Dictionary {"ip":[ports]}
 
 	# To check whether something should occur based of the verbosity.
 	# Eg if something requires high verbosity, is the verbosity high 
@@ -90,7 +100,7 @@ class Scan_Handler:
 		else:
 			if type(threads) == int:
 				if threads > 0:
-					self.Threads = threads
+					self.Threads_Size = threads
 					print("[+] Threads have been updated!")
 					return True
 				else:
@@ -98,3 +108,78 @@ class Scan_Handler:
 			else:
 				print("[!] Threads must be an integer!")
 		return False
+
+	# Scanning
+
+	def Start_Scanner (self, _from, _to):
+		# Check if the inputs are in the IP data type. Convert if not.
+		if type(_from) != IPV4:
+			_from = IPV4(ip=_from)
+		if type(_to) != IPV4:
+			_to = IPV4(ip=_to)
+
+		self.Running = True
+		self.que = Queue() # Reset the Queue
+
+		# Loops through all the addresses untill _from matches _to
+		while True:
+			self.que.put(_from.Get_As_String())
+			if _from == _to:
+				break
+			else:
+				_from.Next_IP()
+
+
+		self.Threads = {} # Reset the Threads
+		for thread in range(self.Thread_Size):
+			self.Threads[thread] = Scanner_Thread(self)
+			self.Threads[thread].Start_Scanning()
+
+		self.que.join()
+		for _ in range(self.Thread_Size):
+			self.que.put(None)
+
+		for thread in self.Threads:
+			self.Threads[thread].thread.join()
+
+			self.Running = False
+
+class Scanner_Thread:
+	def __init__ (self, controller):
+		self.controller = controller # For reference
+		
+		self.socket = socket(AF_INET, SOCK_STREAM) # For scanning
+
+		self.thread = None
+
+	def Scan(self, server, port):
+		try:
+			self.socket.connect((server, port))
+			return True
+		except:
+			return False
+
+	def Start_Scanning (self):
+		self.thread = Thread(target=self.Run_Thread)
+		self.thread.daemon = True
+		self.thread.start()
+
+	def Run_Thread(self):
+		while True:
+			open_ports = []
+			server = self.controller.que.get()
+			if server is None:
+				break
+			for port in self.controller.Ports:
+				if self.Scan(server, port):
+					open_ports.append(port)
+
+			if len(open_ports) > 0:
+				self.controller.Open_Addresses[server] = open_ports
+
+			self.controller.que.task_done()
+
+if __name__ == "__main__":
+	Scanner = Scan_Handler()
+	Scanner.Start_Scanner("192.168.0.1", "192.168.0.20")
+	print(Scanner.Open_Addresses)
